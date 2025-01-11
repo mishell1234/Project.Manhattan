708aa872bc0ce5fed8f8fcbc3a76ecebe776ab75
mishell1234 <157923867+mishell1234@users.noreply.github.com>
2025-01-11 10:25:27 -0500

    PruebaInternoJS.js

--- PruebaInternoJS.js
+++ PruebaInternoJS.js
@@ -1,2 +1,2 @@
 // Fecha: 11.01.25 Prueba: "Abrir pagina del Interno"
-javascript:setTimeout(function(){ window.location.href='https://www.google.com/?gws_rd=ssl'; }, 100);
+javascript:setTimeout(function(){ window.location.href='https://www.google.com/'; }, 100);
