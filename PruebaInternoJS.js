c82ae2141da71dceffc29eeb474c445451a85963
mishell1234 <157923867+mishell1234@users.noreply.github.com>
2025-01-11 10:09:34 -0500

    PruebaInternoJS.js

--- PruebaInternoJS.js
+++ PruebaInternoJS.js
@@ -1,2 +1,2 @@
 // Fecha: 11.01.25 Prueba: "Abrir pagina del Interno"
-javascript:window.open('http://interno39.deltron.com.pe/sys.php/user/login', '_self');
+javascript:window.location.href('http://interno39.deltron.com.pe/sys.php/user/login', '_self');
